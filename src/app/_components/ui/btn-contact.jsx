09e31a5e4ba624ccead8d4aaa68d5096f98d498f
Virtual Epoch <cdk11235@gmail.com>
2024-08-25 19:06:51 -0600

    ...

--- src/app/_components/ui/btn-contact.jsx
+++ src/app/_components/ui/btn-contact.jsx
@@ -24,28 +24,30 @@ export const BtnContact = ({ setModal }) => {
   }, []);
 
   return (
-    <button
-      className={
-        smallBtn ? "btn-open-contact-modal small" : "btn-open-contact-modal"
-      }
-      onClick={() => {
-        setModal(true);
-      }}
-      onMouseDown={() => setPressed(true)}
-      onMouseUp={() => setPressed(false)}
-      onTouchStart={() => setPressed(true)}
-      onTouchEnd={() => setPressed(false)}
-      style={{
-        width: smallBtn ? "50px" : "254px",
-        opacity: smallBtn ? 0.7 : 1,
-        filter: pressed ? "none" : "drop-shadow(0 0 3px #0ff)",
-      }}
-    >
-      <div
-        className={smallBtn ? "btn-contact-text" : "btn-contact-text visible"}
+    <div className="btn-contact-wrap">
+      <button
+        className={
+          smallBtn ? "btn-open-contact-modal small" : "btn-open-contact-modal"
+        }
+        onClick={() => {
+          setModal(true);
+        }}
+        onMouseDown={() => setPressed(true)}
+        onMouseUp={() => setPressed(false)}
+        onTouchStart={() => setPressed(true)}
+        onTouchEnd={() => setPressed(false)}
+        style={{
+          width: smallBtn ? "28px" : "254px",
+          height: smallBtn ? "33px" : "54px",
+          filter: pressed ? "none" : "drop-shadow(0 0 3px #0ff)",
+        }}
       >
-        {smallBtn ? "" : "contact us"}
-      </div>
-    </button>
+        <div
+          className={smallBtn ? "btn-contact-text" : "btn-contact-text visible"}
+        >
+          {smallBtn ? "" : "contact us"}
+        </div>
+      </button>
+    </div>
   );
 };
